c80261dc0fff64da31c74b14d955a74811ffe90e
mahdidolati <mehdidolati@hotmail.com>
2015-07-01 15:37:34 +0430

    add event

---
+++ Event.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mahdi
+ */
+public class Event {
+    double time;
+    int type;
+    QSystem sys;
+    Customer cust;
+    static int ARRIVAL = 0;
+    static int DEPARTURE = 1;
+    public Event(double t, int y, QSystem sys) {
+        this.time = t;
+        this.type = y;
+        this.sys = sys;
+        this.cust = null;
+    }
+}
